86e74169af39321dae8c436171512a21f1d027fd
= <mike@reedell.com>
2010-03-04 21:28:31 -0500

    Refactoring and a lot more rspec's.

--- sunrise.rb
+++ sunrise.rb
@@ -18,89 +18,107 @@ class SolarEventCalculator
   end
 
   def compute_rise_longitude_hour
-    @date.yday + ((BigDecimal.new("6") - compute_longitude_hour) / BigDecimal.new("6"))
+    longHour = @date.yday + ((BigDecimal.new("6") - compute_longitude_hour) / BigDecimal.new("24"))
+    longHour.round(4)
   end
 
-  def compute_sun_mean_anomaly
+  def compute_set_longitude_hour
+    longHour = @date.yday + ((BigDecimal.new("18") - compute_longitude_hour) / BigDecimal.new("24"))
+    longHour.round(4)
+  end
+
+  def compute_sun_mean_anomaly(longHour)
     constant = BigDecimal.new("0.9856")
-    (compute_rise_longitude_hour * constant) - BigDecimal.new("3.289")
+    ((longHour * constant) - BigDecimal.new("3.289")).round(4)
   end
 
-  def compute_sun_true_longitude
-    meanAnomaly = compute_sun_mean_anomaly
+  def compute_sun_true_longitude(meanAnomaly)
     mAsRads = degrees_as_rads(meanAnomaly)
     sinM = BigDecimal.new(Math.sin(mAsRads.to_f).to_s)
     sinTwoM = BigDecimal.new(Math.sin((2 * mAsRads).to_f).to_s)
     firstParens = BigDecimal.new("1.916") * sinM
     secondParens = BigDecimal.new("0.020") * sinTwoM
-    trueLong = meanAnomaly + firstParens + secondParens + 282.634
-
-    put_in_range(trueLong, 0, 360, 360)
+    trueLong = meanAnomaly + firstParens + secondParens + BigDecimal.new("282.634")
+    trueLong = put_in_range(trueLong, 0, 360, 360)
+    trueLong.round(4)
   end
 
-  def compute_right_ascension
-    tanL = BigDecimal.new(Math.tan(degrees_as_rads(compute_sun_true_longitude).to_f).to_s)
+  def compute_right_ascension(sunTrueLong)
+    tanL = BigDecimal.new(Math.tan(degrees_as_rads(sunTrueLong).to_f).to_s)
     ra = rads_as_degrees(BigDecimal.new(Math.atan(BigDecimal.new("0.91764") * tanL).to_s))
 
-    put_in_range(ra, 0, 360, 360)
+    ra = put_in_range(ra, 0, 360, 360)
+    ra.round(4)
   end
 
-  def put_in_range(number, lower, upper, adjuster)
-    if number > upper then
-      number -= adjuster
-    elsif number < lower then
-      number += adjuster
-    else
-      number
-    end
-  end
+  def put_ra_in_correct_quadrant(sunTrueLong)
+    lQuadrant = BigDecimal.new("90") * (sunTrueLong / BigDecimal.new("90")).floor
+    raQuadrant = BigDecimal.new("90") * (compute_right_ascension(sunTrueLong) / BigDecimal.new("90")).floor
 
-  def put_ra_in_correct_quadrant
-    lQuadrant = BigDecimal.new("90") * (compute_sun_true_longitude / BigDecimal.new("90")).floor
-    raQuadrant = BigDecimal.new("90") * (compute_right_ascension / BigDecimal.new("90")).floor
-
-    ra = compute_right_ascension + (lQuadrant - raQuadrant)
+    ra = compute_right_ascension(sunTrueLong) + (lQuadrant - raQuadrant)
     ra = ra / BigDecimal.new("15")
+    ra.round(4)
   end
 
-  def compute_sin_sun_declination
-    sinL = BigDecimal.new(Math.sin(degrees_as_rads(compute_sun_true_longitude).to_f).to_s)
+  def compute_sin_sun_declination(sunTrueLong)
+    sinL = BigDecimal.new(Math.sin(degrees_as_rads(sunTrueLong).to_f).to_s)
     sinDec = sinL * BigDecimal.new("0.39782")
+    sinDec.round(4)
   end
 
-  def compute_cosine_sun_declination
-    BigDecimal.new(Math.cos(Math.asin(compute_sin_sun_declination)).to_s)
+  def compute_cosine_sun_declination(sinSunDeclination)
+    cosDec = BigDecimal.new(Math.cos(Math.asin(sinSunDeclination)).to_s)
+    cosDec.round(4)
   end
 
-  def compute_cosine_sun_local_hour
-    cosZenith = BigDecimal.new(Math.cos(degrees_as_rads(BigDecimal.new("96"))).to_s)
+  def compute_cosine_sun_local_hour(sunTrueLong, zenith)
+    cosZenith = BigDecimal.new(Math.cos(degrees_as_rads(BigDecimal.new(zenith.to_s))).to_s)
     sinLatitude = BigDecimal.new(Math.sin(degrees_as_rads(@latitude)).to_s)
     cosLatitude = BigDecimal.new(Math.cos(degrees_as_rads(@latitude)).to_s)
 
-    top = cosZenith - (compute_sin_sun_declination * sinLatitude)
-    bottom = compute_cosine_sun_declination * cosLatitude
+    sinSunDeclination = compute_sin_sun_declination(sunTrueLong)
+    top = cosZenith - (sinSunDeclination * sinLatitude)
+    bottom = compute_cosine_sun_declination(sinSunDeclination) * cosLatitude
+
     cosLocalHour = top / bottom
+    cosLocalHour.round(4)
   end
 
-  def compute_local_hour_angle
-    acosH = Math.acos(compute_cosine_sun_local_hour)
+  def compute_local_hour_angle(cosSunLocalHour)
+    acosH = Math.acos(cosSunLocalHour)
     localHourAngle = BigDecimal.new("360") - rads_as_degrees(acosH)
     localHourAngle = localHourAngle / BigDecimal.new("15")
   end
 
-  def compute_local_mean_time
-    h = compute_local_hour_angle
-    ra = put_ra_in_correct_quadrant
+  def compute_set_local_hour_angle
+    acosH = Math.acos(compute_cosine_sun_local_hour)
+    localHourAngle = acosH / BigDecimal.new("15")
+  end
+
+  def compute_local_mean_time(sunTrueLong, longHour, sunLocalHour)
+    h = sunLocalHour
+    ra = put_ra_in_correct_quadrant(sunTrueLong)
     t = compute_rise_longitude_hour
 
     parens = BigDecimal.new("0.06571") * t
     time = h + ra - parens - BigDecimal.new("6.622")
 
-    utcTime = time - compute_longitude_hour
+    utcTime = time - longHour
   end
 
-  def compute_utc_sunrise
-    timeParts = compute_local_mean_time.to_s.split('.')
+  def compute_utc_sunrise(zenith)
+    longHour = compute_longitude_hour
+
+    meanAnomaly = compute_sun_mean_anomaly(longHour)
+    sunTrueLong = compute_sun_true_longitude(meanAnomaly)
+    cosineSunLocalHour = compute_cosine_sun_local_hour(sunTrueLong, zenith)
+
+    #put logic here for handling when sun doesn't rise/set.
+
+    sunLocalHour = compute_local_hour_angle(cosineSunLocalHour)
+    localMeanTime = compute_local_mean_time(sunTrueLong, longHour, sunLocalHour)
+
+    timeParts = localMeanTime.to_s.split('.')
     mins = BigDecimal.new("." + timeParts[1]) * BigDecimal.new("60")
     mins = mins.truncate()
 
@@ -115,6 +133,16 @@ class SolarEventCalculator
   def pad_minutes(minutes)
     if(minutes < 10)
       "0" + minutes.to_s
+    end
+  end
+
+  def put_in_range(number, lower, upper, adjuster)
+    if number > upper then
+      number -= adjuster
+    elsif number < lower then
+      number += adjuster
+    else
+      number
     end
   end
 
--- sunrisetest.rb
+++ sunrisetest.rb
@@ -1,9 +1,82 @@
 require 'sunrise'
 
-describe SolarEventCalculator, "#sunrise time for 01 Nov 2009 @home" do
-  it "returns correct time" do
-    date = Date.parse('2009-11-01')
+describe SolarEventCalculator, "sunrise longitude hour for 01 Nov 2008 @home" do
+  it "returns correct longitude hour" do
+    date = Date.parse('2008-11-01')
     calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
-    calc.compute_utc_sunrise.should.to_s == "Sun Nov 01 11:05:00 UTC 2009"
+    calc.compute_rise_longitude_hour.should eql(BigDecimal.new("306.4605"))
   end
 end
+
+describe SolarEventCalculator, "sunset longitude hour for 01 Nov 2008 @home" do
+  it "returns correct longitude hour" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_set_longitude_hour.should eql(BigDecimal.new("306.9605"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise mean anomaly" do
+  it "returns correct sunrise mean anomaly" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_sun_mean_anomaly(BigDecimal.new("306.4605")).should eql(BigDecimal.new("298.7585"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise sun true longitude" do
+  it "returns correct sunrise's sun true longitude" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_sun_true_longitude(BigDecimal.new("298.7585")).should eql(BigDecimal.new("219.6960"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise right ascension" do
+  it "returns correct sunrise's right ascension" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_right_ascension(BigDecimal.new("219.6960")).should eql(BigDecimal.new("37.2977"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise right ascension in correct quadrant" do
+  it "returns correct sunrise's right ascension quadrant" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.put_ra_in_correct_quadrant(BigDecimal.new("219.6960")).should eql(BigDecimal.new("14.4865"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise sin sun declination" do
+  it "returns correct sunrise sin sun declination" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_sin_sun_declination(BigDecimal.new("219.6960")).should eql(BigDecimal.new("-0.2541"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise cosine sun declination" do
+  it "returns correct sunrise cosine sun declination" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_cosine_sun_declination(BigDecimal.new("-0.2541")).should eql(BigDecimal.new("0.9672"))
+  end
+end
+
+describe SolarEventCalculator, "sunrise cosine sun local hour" do
+  it "returns correct sunrise cosine sun local hour" do
+    date = Date.parse('2008-11-01')
+    calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+    calc.compute_cosine_sun_local_hour(BigDecimal.new("219.6960"), 96).should eql(BigDecimal.new("0.0791"))
+  end
+end
+
+
+# describe SolarEventCalculator, "sunrise time for 01 Nov 2009 @home" do
+#   it "returns correct time" do
+#     date = Date.parse('2008-11-01')
+#     calc = SolarEventCalculator.new(date, BigDecimal.new("39.9537"), BigDecimal.new("-75.7850"))
+#     calc.compute_utc_sunrise(96).should eql("Sun Nov 01 11:05:00 UTC 2009")
+#   end
+# end
